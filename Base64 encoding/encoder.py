4c4b570feaec2b0a7e173054c1710a7922236e41
dipson <dipson94@gmail.com>
2023-04-27 11:21:26 +0200

    added file write

--- Base64 encoding/encoder.py
+++ Base64 encoding/encoder.py
@@ -1,5 +1,6 @@
 import base64
-path=str(input("enter full file path"))
+
+path = input("Enter the full file path: ")
 
 with open(path, "rb") as f:
     # read the contents of the file as bytes
@@ -12,3 +13,6 @@ base64_bytes = base64.b64encode(file_bytes)
 base64_string = base64_bytes.decode('utf-8')
 
 print(base64_string)
+
+with open("output.txt", "wb") as f:
+    f.write(base64_string.encode('utf-8'))
