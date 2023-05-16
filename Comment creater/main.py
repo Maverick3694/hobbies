f86aafeef155912f74d84f60ae8eb4dfbb25b651
dipson <dipson94@gmail.com>
2023-05-16 04:14:22 +0200

    update

--- Comment creater/main.py
+++ Comment creater/main.py
@@ -1,11 +1,26 @@
+
+"""
+The program generates comment template for commenting python programs.
+By taking comment values and length of comment template.
+eg: 
+#----------------------------------------------comment------------------------------------------------
+Here comment length is 100 characters and value is "comment".
+Subsequently the comment is copied to clipboard.
+"""
+#-----------------------------------------------START-------------------------------------------------
+
 import pyperclip
+
 n=int(input("\nenter the length : "))
 loop=True
 while(loop==True):
     c=str(input("\nenter the comment: "))
+    if len(c)>(n-2):
+        print("\n ! The length of comment value is larger than the template Value")
+        n=int(input("\nenter new length : "))
+        continue
     m=n/2
     m=m-(len(c)/2)
-    m=m-1
     comment="#"
     i=0
     while(i<n):
@@ -20,7 +35,16 @@ while(loop==True):
             i=i+1
     comment="\n"+comment+"\n"
     print(comment)
+
+#-----------------------------------------Copy to clipboard-------------------------------------------
+
     pyperclip.copy(comment)
+
+#---------------------------------------------Loop Bool-----------------------------------------------
+
     boolean=input("\nanother comment ? (y/n) ")
     if boolean=="n" or boolean=="N":
         loop=False
+
+#------------------------------------------------END--------------------------------------------------
+
